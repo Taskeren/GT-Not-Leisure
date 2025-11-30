5d5c46ad6f47248c739e5fc65c6e787e82c25bcb
Circulate233 <a18966009058@163.com>
2025-11-30 19:58:52 +0800

    修复有量子增幅卡的无线终端不能在没有无线访问点的情况下使用中键获取方块功能的bug

--- src/main/java/com/science/gtnl/mixins/late/RandomComplement/MixinWirelessTerminalGuiObject.java
+++ src/main/java/com/science/gtnl/mixins/late/RandomComplement/MixinWirelessTerminalGuiObject.java
@@ -10,14 +10,11 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import com.glodblock.github.util.Util;
 import com.science.gtnl.utils.RCWirelessTerminalGuiObject;
 
 import appeng.api.features.IWirelessTermHandler;
-import appeng.api.implementations.tiles.IWirelessAccessPoint;
 import appeng.helpers.WirelessTerminalGuiObject;
 
 @SuppressWarnings("AddedMixinMembersNamePattern")
@@ -36,7 +33,7 @@ public abstract class MixinWirelessTerminalGuiObject implements RCWirelessTermin
 
     @Inject(method = "<init>", at = @At("TAIL"))
     public void onInit(IWirelessTermHandler wh, ItemStack is, EntityPlayer ep, World w, int x, int y, int z,
-        CallbackInfo ci) {
+                       CallbackInfo ci) {
         if (z == Integer.MIN_VALUE) {
             gtnl$isSpecial = true;
             gtnl$isBauble = y == 1;
@@ -49,13 +46,6 @@ public abstract class MixinWirelessTerminalGuiObject implements RCWirelessTermin
     @Override
     public abstract int getInventorySlot();
 
-    @Shadow
-    public abstract boolean testWap(IWirelessAccessPoint wap);
-
-    @Shadow
-    @Final
-    private ItemStack effectiveItem;
-
     @Unique
     @Override
     public boolean isBauble() {
@@ -66,14 +56,5 @@ public abstract class MixinWirelessTerminalGuiObject implements RCWirelessTermin
     @Override
     public boolean isSpecial() {
         return gtnl$isSpecial;
-    }
-
-    @Redirect(
-        method = "rangeCheck",
-        at = @At(
-            value = "INVOKE",
-            target = "Lappeng/helpers/WirelessTerminalGuiObject;testWap(Lappeng/api/implementations/tiles/IWirelessAccessPoint;)Z"))
-    public boolean rangeCheck(WirelessTerminalGuiObject instance, IWirelessAccessPoint wap) {
-        return this.testWap(wap) || Util.hasInfinityBoosterCard(this.effectiveItem);
     }
 }
