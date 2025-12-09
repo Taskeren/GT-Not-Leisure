d3b327b36baf7c331319be21a8e75c770d965ca5
ABKQPO <93412322+ABKQPO@users.noreply.github.com>
2025-12-09 17:19:42 +0800

    Update MegaSteamCompressor.java

--- src/main/java/com/science/gtnl/common/machine/multiblock/steam/MegaSteamCompressor.java
+++ src/main/java/com/science/gtnl/common/machine/multiblock/steam/MegaSteamCompressor.java
@@ -4,7 +4,6 @@ import static com.gtnewhorizon.structurelib.structure.StructureUtility.*;
 import static com.science.gtnl.ScienceNotLeisure.RESOURCE_ROOT_ID;
 import static com.science.gtnl.utils.enums.BlockIcons.*;
 import static gregtech.api.GregTechAPI.*;
-import static gregtech.api.enums.GTValues.V;
 import static gregtech.api.enums.HatchElement.*;
 import static gregtech.api.recipe.RecipeMaps.compressorRecipes;
 import static gregtech.api.util.GTStructureUtility.*;
@@ -226,14 +225,6 @@ public class MegaSteamCompressor extends SteamMultiMachineBase<MegaSteamCompress
     @Override
     public int getMaxParallelRecipes() {
         return 256;
-    }
-
-    @Override
-    public void setProcessingLogicPower(ProcessingLogic logic) {
-        logic.setAvailableVoltage(V[3]);
-        // We need to trick the GT_ParallelHelper we have enough amps for all recipe parallels.
-        logic.setAvailableAmperage(getTrueParallel());
-        logic.setAmperageOC(false);
     }
 
     @Override
