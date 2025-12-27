716dabc2faedc90eb32164d3f38d542eda31f568
Psi <Psi@123.com>
2025-12-27 16:18:45 +0800

    修改铂系金属处理机的配方

--- src/main/java/com/science/gtnl/common/recipe/gtnl/PlatinumBasedTreatmentRecipes.java
+++ src/main/java/com/science/gtnl/common/recipe/gtnl/PlatinumBasedTreatmentRecipes.java
@@ -147,11 +147,11 @@ public class PlatinumBasedTreatmentRecipes implements IRecipePool {
 
         // 钯金属粉处理
         RecipeBuilder.builder()
-            .itemInputs(GTUtility.copyAmountUnsafe(90, WerkstoffLoader.PDMetallicPowder.get(OrePrefixes.dust, 1)))
+            .itemInputs(GTUtility.copyAmountUnsafe(90, WerkstoffLoader.PDMetallicPowder.get(OrePrefixes.dust, 1)),GTOreDictUnificator.get(OrePrefixes.dust, Materials.Carbon, 64))
             .itemOutputs(GTOreDictUnificator.get(OrePrefixes.dust, Materials.Palladium, 64))
             .fluidInputs(
                 Materials.Hydrogen.getGas(130000),
-                Materials.CarbonMonoxide.getGas(64000),
+                Materials.Oxygen.getGas(64000),
                 Materials.Ammonia.getGas(224000))
             .fluidOutputs(Materials.Ethylene.getGas(64000))
             .duration(2400)
